8d364fc35658f8cdc58c703246b182c38a78e797
Eduardo <eduardopferreira249@gmail.com>
2026-07-06 18:54:01 -0300

    metodos nas structs

--- src/rust_boot_anotacoes/21_Struct_Metodo.rs
+++ src/rust_boot_anotacoes/21_Struct_Metodo.rs
@@ -51,3 +51,60 @@ fn main() {
         validar
     );
 }
+
+// Exemplo hsa da 98
+fn main() {
+  
+  let peca: Peca = Peca {
+    id: 1,
+    codigo_pi: String::from("15570080"),
+    nome: String::from("31-2298 - Haste Curvada"),
+    proxima_operacao: ProximaOperacao::Recalcar,
+    data_entrada: String::from("01/07/2026"),
+    lote: String::from("H0501001001"),
+    data_saida: None,
+  };
+  
+  let status = match &peca.data_saida {   
+    Some(data) => format!("Saiu em: {}", data),
+    None       => String::from("Em estoque"),
+};
+
+  println!("{:#?}", peca);
+  println!("Status: {}", status);
+
+}
+
+// Exemplo de puadas de função
+
+fn main() {
+    let conn = Connection::open("pecas.db").expect("Erro ao abrir banco");
+
+    iniciar_banco(&conn);
+
+    let peca = Peca {
+        id: 0, // o banco vai gerar o id real
+        codigo_pi:        String::from("15570080"),
+        nome:             String::from("HSA - Haste Curvada"),
+        proxima_operacao: ProximaOperacao::Recalcar,
+        lote:             String::from("H0501001001"),
+        data_entrada:     String::from("01/07/2026"),
+        data_saida:       None,
+    };
+
+    inserir_peca(&conn, &peca);
+    
+    // atualizar_peca(&conn, &peca_atualizada);
+
+    // deletar_peca(&conn, );
+
+    let lista = listar_pecas(&conn);
+    for p in &lista {
+        let status = match &p.data_saida {
+            Some(data) => format!("Saiu em: {}", data),
+            None       => String::from("Em estoque"),
+        };
+        println!("#{} | {} | {} | {} | {} | {}", p.id, p.codigo_pi, p.nome, p.lote, p.data_entrada, status);
+    }
+
+}
